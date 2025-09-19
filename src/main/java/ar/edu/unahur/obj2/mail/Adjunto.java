687a83b0a12a67a776b90afd82161b6166fdb800
Franco2912 <francoscout13@hotmail.com>
2025-09-19 20:16:01 -0300

    Parte 2, falta terminar

--- src/main/java/ar/edu/unahur/obj2/mail/Adjunto.java
+++ src/main/java/ar/edu/unahur/obj2/mail/Adjunto.java
@@ -1,3 +1,7 @@
 public class Adjunto extends FiltroSimple{
 
+    @override
+    public Boolean doAplicar(){
+        return mail.getAdjunto.contains(texto);
+    } 
 }
--- src/main/java/ar/edu/unahur/obj2/mail/Mail.java
+++ src/main/java/ar/edu/unahur/obj2/mail/Mail.java
@@ -7,9 +7,19 @@ public class Mail {
     private String asunto;
     private Integer tamano;
     private Boolean adjunto;
+    private FiltroDefault filtro;
 
     public Mail(String from, String to, String Asunto, Integer tamano, Boolean adjunto){
+        this.from = from;
+        this.to = to;
+        this.asunto = asunto;
+        this.tamano = tamano;
+        this.adjunto = adjunto;
 
     }
     /*Agregar los getter, setter y constructor */
+
+    public boolean filtrate(){
+        return filtro.aplicar();
+    }
 }
